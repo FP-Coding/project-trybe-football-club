87888ec4f6133be8707334f0a3daa773670611d3
Felipe Pinto <felipe14062000@gmail.com>
2023-02-27 16:09:05 -0300

    feat: criação das migration Team e da model Team

---
+++ app/backend/src/database/migrations/20230227182544-teams.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('teams');
+  },
+};
--- app/backend/src/database/migrations/99999999999999-create-z.js
+++ app/backend/src/database/migrations/99999999999999-create-z.js
@@ -11,4 +11,3 @@ module.exports = {
     await queryInterface.dropTable('trybe_eval');
   },
 };
-// initial commit
